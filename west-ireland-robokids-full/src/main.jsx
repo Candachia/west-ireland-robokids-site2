13dd48fcd6c619acce51dcb7f0fbab7883901087
Candachia <candachia.sandel@gmail.com>
2025-03-30 19:42:02 +0100

    Add files via upload

---
+++ west-ireland-robokids-full/src/main.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RobokidsSite from './RobokidsSite';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RobokidsSite />
+  </React.StrictMode>
+);
